673e51424a2efe20883341f23327a01f11a3c14d
Peter Mårtensson <amatex100@gmail.com>
2017-02-01 22:36:26 +0100

    Changed layout Added core data added navigation

---
+++ MyTravelApp/DataModelController.swift
@@ -0,0 +1,9 @@
+//
+//  DataModelController.swift
+//  MyTravelApp
+//
+//  Created by Peter Mårtensson on 2017-02-01.
+//  Copyright © 2017 Peter Mårtensson. All rights reserved.
+//
+
+import Foundation
